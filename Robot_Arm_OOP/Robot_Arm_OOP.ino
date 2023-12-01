6e24dfb8456fbd3e897b98920f1fa921a3147cac
Puen Xu <xupuen1223@gmail.com>
2023-12-01 09:56:18 -0500

    fixed command

--- Robot_Arm_OOP/Robot_Arm_OOP.ino
+++ Robot_Arm_OOP/Robot_Arm_OOP.ino
@@ -43,14 +43,14 @@ void setup() {
     // joint3.setSpeed(1000);
 
     // turn on gripper
-    gripper.setup();
+    // gripper.setup();
 }
 
 // initial desired position
 int j1_desiredPos = 0;
 int j2_desiredPos = 0;
 int j3_desiredPos = 0;
-int gripper_desiredPos = 0;
+int gripper_desiredPos = 180;
 
 void loop() {
 
@@ -69,27 +69,47 @@ void loop() {
         ptr = strtok(nullptr, ",");
       }
 
-      j1_desiredPos = values[0];
-      j2_desiredPos = values[1];
-      j3_desiredPos = values[2];
-      gripper_desiredPos = values[3];    
+      for (int i = 0; i < 4; i++){
 
-      Serial.print("Joint1: ");
-      Serial.print(j1_desiredPos);
-      Serial.print("  Joint2: ");
-      Serial.print(j2_desiredPos);
-      Serial.print("  Joint3: ");
-      Serial.print(j3_desiredPos);
-      Serial.print("  Gripper: ");
-      Serial.println(gripper_desiredPos);
+        if (i < 3){
+          if (values[i] >= -90 && values[i] <= 90){}
+          else{ 
+            Serial.println("Warning: joints values should be from -90 to 90");
+            break; 
+          }
+        }
+
+        if (i == 3){
+          if (values[3] >= 0 && values[3] <= 180){
+            
+            j1_desiredPos = float(values[0] / 180.0) * 10000;
+            j2_desiredPos = float(values[1] / 180.0) * 10000;
+            j3_desiredPos = float(values[2] / 180.0) * 10000;
+            gripper_desiredPos = values[3];    
+
+            Serial.print("Joint1: ");
+            Serial.print(j1_desiredPos);
+            Serial.print("  Joint2: ");
+            Serial.print(j2_desiredPos);
+            Serial.print("  Joint3: ");
+            Serial.print(j3_desiredPos);
+            Serial.print("  Gripper: ");
+            Serial.println(gripper_desiredPos);
+          }
+          else{ 
+            Serial.println("Warning: gripper values should be from 0 to 180");
+            break; 
+          }
+        }
+      }
     }
 
-    int pos = gripper.readPos();
-    Serial.print("Analog Reading: ");
-    Serial.println(pos);
+    // int pos = gripper.readPos();
+    // Serial.print("Analog Reading: ");
+    // Serial.println(pos);
 
     // motor the gripper and joints
-    gripper.moveTo(gripper_desiredPos);
+    // gripper.moveTo(gripper_desiredPos);
     // joint1.moveTo(j1_desiredPos);
     // joint2.moveTo(j2_desiredPos);
     // joint3.moveTo(j3_desiredPos);
